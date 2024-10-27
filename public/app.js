4b36c6d47aefcf237df0fca0f7f598aa92d14d71
Juan Manuel <juanmanuelamado00@outlook.com>
2024-10-27 08:44:57 -0300

    Navbar_Terminado

--- public/app.js
+++ public/app.js
@@ -1,4 +1,7 @@
 function toggleMenu() {
   let burger = document.querySelector(".burger");
   burger.classList.toggle("open");
+  const list = document.querySelector(".menu__links");
+
+  list.classList.toggle("menu__links--show");
 }
