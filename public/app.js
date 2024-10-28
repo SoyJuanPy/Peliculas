97e10a1a1b69551b42b0785b8c6cc3c01fa97273
Juan Manuel <juanmanuelamado00@outlook.com>
2024-10-27 23:08:18 -0300

    url

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ function toggleMenu() {
 
   list.classList.toggle("menu__links--show");
 }
-fetch("https://api.themoviedb.org/3/movie/popular")
+fetch("https://peliculas-murex.vercel.app/api/movies")
   .then((response) => {
     if (!response.ok) {
       throw new Error("Error en la respuesta de la API");
