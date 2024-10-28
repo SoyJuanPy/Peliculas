8e2ea4ac53683362771ebb0e03df83baab7e2e15
Juan Manuel <juanmanuelamado00@outlook.com>
2024-10-27 23:16:00 -0300

    Fix API routing

--- server/index.js
+++ server/index.js
@@ -3,16 +3,16 @@ const axios = require("axios");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../public"))); // Sirve archivos estáticos desde la carpeta public
+app.use(express.static(path.join(__dirname, "../public")));
 
-// Ruta para consumir la API de películas
+// Ruta para consumir la API de Magic Loops
 app.get("/api/movies", async (req, res) => {
   try {
     const response = await axios.get(
       "https://api.themoviedb.org/3/movie/popular",
       {
         params: {
-          api_key: "ed580b25b58102be44c94151cda257c0", // Usa tu API key
+          api_key: "ed580b25b58102be44c94151cda257c0",
           page: 1,
         },
       }
@@ -20,15 +20,13 @@ app.get("/api/movies", async (req, res) => {
     const movies = response.data.results.slice(0, 50); // Obtener las primeras 50 películas
     res.json(movies);
   } catch (error) {
-    console.error(error);
     res
       .status(500)
       .json({ error: "Hubo un problema al conectar con la API de películas" });
   }
 });
 
-// Escucha en el puerto definido por Vercel
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor en ejecución en el puerto ${PORT}`);
+  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
 });
